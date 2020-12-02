cabb0068e0a9655025ae67be276424b629c2c630
skeyen <stevekeyen@gmail.com>
2020-12-02 09:41:20 +0100

    add material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<mat-slider min="1" max="100" step="1" value="1"></mat-slider>`
 })
 export class AppComponent {
-  title = 'keyenWebsite';
+  title = 'Targetworx';
 }
